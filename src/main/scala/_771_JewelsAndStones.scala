828b0834c0cf34c9156ed2c7983ef6b46820af31
Fuma_Tsuchiya <korezonzip@gmail.comm>
2019-12-03 01:36:12 +0900

    accepted

---
+++ src/main/scala/_771_JewelsAndStones.scala
@@ -0,0 +1,14 @@
+object _771_JewelsAndStones {
+  def main(args: Array[String]): Unit = {
+    println((numJewelsInStones(("aA"),("aaAbbzon"))))
+  }
+  def numJewelsInStones(J: String, S: String): Int = {
+    val sList = S.split("")
+    val jList = J.split("")
+    var result = 0
+    for(x <- jList){
+      result += sList.count( y => y.contains(x))
+    }
+    result
+  }
+}
