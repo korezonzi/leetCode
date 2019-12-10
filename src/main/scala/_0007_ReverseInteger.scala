268186546b0514a0bddf589c01167a7b71d083d4
fuma-tsuchiya <fuma.tsuchiya@nextbeat.net>
2019-12-10 21:20:50 +0900

    Accepted

--- src/main/scala/_0007_ReverseInteger.scala
+++ src/main/scala/_0007_ReverseInteger.scala
@@ -1,10 +1,12 @@
+//Accepted
+//Memory Usage: 42.3 MB Runtime: 240 ms
+
 object _0007_ReverseInteger {
    def main(args: Array[String]): Unit = {
-    println(reverse(1499309429))
-  }
-
-  def isRange(x: Int): Boolean = {
-    x <=  2147483647 || x >= -2147483648
+     println(reverse(1534236469))
+     println(reverse(15336469))
+     println(reverse(-15336469))
+     println(reverse(-153364690))
   }
 
   def reverse(x: Int): Int = {
@@ -12,13 +14,16 @@ object _0007_ReverseInteger {
       //Stringに合わせるため
       if(x == 0 ) 0.toString
       else{ x.toString.toList  match {
-        case y     if(y.endsWith(("0")) && y.startsWith("-")) => ("-" +: y.init.drop((1)).reverse).mkString("")
+        case y     if(y.endsWith(("0")) && y.startsWith("-")) => ("-" +: y.init.tail.reverse).mkString("")
         case zero  if((zero.endsWith("0")))                   => zero.init.reverse.mkString("")
         case minus if(minus.startsWith("-"))                  => ("-" +: minus.drop(1).reverse).mkString("")
         case x                                                => x.reverse.mkString("")
       }
     }
-    if(isRange(numList.toInt)) numList.toInt
-    else 0
+    if(isRange(numList.toLong)) numList.toInt else 0
+  }
+
+  def isRange(x: Long): Boolean = {
+    x <=  Int.MaxValue && x >= Int.MinValue
   }
 }
