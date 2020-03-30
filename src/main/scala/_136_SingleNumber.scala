aad886615d5a9f37ea8f31cf7349eece04660b37
Fuma_Tsuchiya <korezonzip@gmail.comm>
2020-03-31 03:13:49 +0900

    accepted

--- src/main/scala/_136_SingleNumber.scala
+++ src/main/scala/_136_SingleNumber.scala
@@ -1,3 +1,7 @@
+//Status: Accepted
+//Runtime: 508 ms
+//Memory Usage: 55.2 MB
+
 class _136_SingleNumber {
     def singleNumber(nums: Array[Int]): Int = {
         nums.reduce((v1, v2) => ~(v1 ^ v2))
